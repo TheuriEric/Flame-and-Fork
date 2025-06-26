807e59d34e3cc95f21dbb7d0395f222c28808465
Ephrata <ephrata0327@gmail.com>
2025-06-26 14:05:46 +0300

    Restructure project: move input.css to src, HTML/JS to build, set up Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", 
-           "./js/**/*.js"],
+  content: ["./build/*.html",
+              "./build/*.js"],
   theme: {
     extend: {},
   },
